cf3c486ac8a7475137a76466a576bfb0edc3f95b
Ellie Bogner <ellie.bogner@gmail.com>
2024-02-21 22:03:11 +0100

    Fix Unit test makefile

---
+++ testing/test/server/vault_test.c
@@ -0,0 +1,15 @@
+#include "../../unity/unity.h"
+#include "../../../src/server/vault.h"
+
+void setUp() {
+	//setup
+}
+
+void tearDown() {
+    //clean up
+}
+
+int main() {
+    UNITY_BEGIN();
+    return UNITY_END();
+}
